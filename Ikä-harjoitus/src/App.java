8f6515ef64109febfa9686d0d0f73f7fb999b4d8
JuhoXD <129332320+JuhoXD@users.noreply.github.com>
2025-10-21 20:10:42 +0300

    Add files via upload

---
+++ Ikä-harjoitus/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Voit vaihtaa ikää tässä tai syöttää sen komentoriviltä
+        int ika = 20;
+
+        // Tulostusehdot
+        if (ika >= 0 && ika < 18)
+        {
+            System.out.println("Olet alaikäinen");
+        }
+        else
+        {
+            System.out.println("Olet aikuinen");
+        }
+
+    }
+
+
+
+}
