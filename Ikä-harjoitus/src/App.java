d9a3f17fac78a1deba1c89630e55257d05ffbdb9
JuhoXD <129332320+JuhoXD@users.noreply.github.com>
2025-10-21 20:16:26 +0300

    Add files via upload
    Lisäsin koodiin alaikäisten ja aikuisten lisäksi eläkeläiset

--- Ikä-harjoitus/src/App.java
+++ Ikä-harjoitus/src/App.java
@@ -1,12 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        // Voit vaihtaa ikää tässä tai syöttää sen komentoriviltä
+        // Voit vaihtaa ikää tässä 
         int ika = 20;
 
         // Tulostusehdot
-        if (ika >= 0 && ika < 18)
+        if (ika > 0 && ika < 18)
         {
             System.out.println("Olet alaikäinen");
+        }
+        else if (ika >= 65)
+        {
+            System.out.println("Olet eläkeläinen");
         }
         else
         {
